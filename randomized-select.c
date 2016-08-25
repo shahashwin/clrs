4a9d02184e0451d7512c58f03d3bf9e91bee36fb
shahashwin <khurki@yahoo.com>
2016-08-24 20:35:28 -0700

    Add files via upload

--- randomized-select.c
+++ randomized-select.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 
+/*
+ * Fixes: 
+ * pass elements: num_elem -1
+ * set p to p+1 in random_partition
+ */
+void
+print_array(int *a, int num_elem)
+{
+	int i;
+
+	for(i = 0; i < num_elem; i++) {
+		printf("a[%d] = %d ", i, a[i]);
+	}
+	printf("\n");
+}
+
 void
 swap(int *a, int *b)
 {
@@ -25,8 +41,10 @@ partition(int A[], int p, int r)
 	       if (A[j] < x) {
 			swap(&A[j], &A[++i]);
 	 	}
+	       j++;
 	}
 	swap(&A[++i], &A[r]);
+	printf("%d\n", i);
 	return i;
 }
 
@@ -35,8 +53,11 @@ random_partition(int A[], int p, int r)
 {
 	int i;
 
-	i = ((rand() % p) + p) % r;
+	printf("here\n");
+	i = (rand() | p) % r;
+	printf("%d %d %d\n", i, p, r);
 	swap(&A[i], &A[r]);
+	print_array(A, r-p+1);
 	return partition(A, p, r);
 }
 
@@ -45,21 +66,21 @@ random_select(int A[], int p, int r, int i)
 {
 	int k, q;
 
-	if (p == r)
+	printf("p=%d r=%d\n", p, r);
+	if(p >= r)
 		return A[p];
 
-	q = random_partition(A, p , r);
-	k = p - q + 1;
+	q = random_partition(A, p, r);
+	k = q - p + 1;
 
-	if (i == k)
-		return A[q];
+	printf("i = %d k: %d p: %d q = %d\n", i, k, p, q);
 
-	if (i < k)
+	if(i < k)
 		random_select(A, p, q-1, i);
-	else
+	else if(i > k)
 		random_select(A, q+1, r, i);
 
-	return -1;
+	return A[q];
 }
 
 int
@@ -80,7 +101,8 @@ main(int argc, char *argv[])
 		a[i] = strtoul(argv[i + 2], NULL, 0);
 	}
 
-	i = random_select(a, 0, num_elem, 1);
+
+	i = random_select(a, 0, num_elem-1, 1);
 	printf("The output of random_selec is : %d\n", i);
 
 	return 0;
